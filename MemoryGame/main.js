1aff6c744375736b47be8a32049939ea877949f6
Dan Finelli <Dan@Dans-MacBook-Pro.local>
2016-10-20 16:38:40 -0700

    More game logic

--- MemoryGame/main.js
+++ MemoryGame/main.js
@@ -2,3 +2,17 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+var result;
+
+if (cardTwo === cardFour)
+{ result = true;}
+else {result = false;}
+
+if (result == true)
+{
+	alert("You found a match!");
+}
+else if (result == false)
+{
+	alert("Sorry, Try Again!");
+}
